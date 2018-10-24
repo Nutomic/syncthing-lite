9acc0914201cafba77a99501d758a81e9a1f0d3a
Jonas L <jonas@determapp.de>
2018-10-24 17:53:13 +0200

    Disable https verification in GlobalDiscoveryUtil

--- syncthing-discovery/src/main/kotlin/net/syncthing/java/discovery/protocol/GlobalDiscoveryUtil.kt
+++ syncthing-discovery/src/main/kotlin/net/syncthing/java/discovery/protocol/GlobalDiscoveryUtil.kt
@@ -23,12 +23,33 @@ import java.io.IOException
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.HostnameVerifier
+import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLContext
+import javax.net.ssl.X509TrustManager
 
 object GlobalDiscoveryUtil {
     suspend fun queryAnnounceServer(server: String, deviceId: DeviceId): AnnouncementMessage {
         return withContext(Dispatchers.IO) {
             val url = URL("https://$server/v2/?device=${deviceId.deviceId}")
-            val connection = url.openConnection() as HttpURLConnection
+            val connection = (url.openConnection() as HttpsURLConnection).apply {
+                hostnameVerifier = HostnameVerifier { _, _ -> true }
+                sslSocketFactory = SSLContext.getInstance("SSL").apply {
+                    init(null, arrayOf(object: X509TrustManager {
+                        override fun checkClientTrusted(p0: Array<out X509Certificate>?, p1: String?) {
+                            // check nothing
+                        }
+
+                        override fun checkServerTrusted(p0: Array<out X509Certificate>?, p1: String?) {
+                            // check nothing
+                        }
+
+                        override fun getAcceptedIssuers(): Array<X509Certificate> = emptyArray()
+                    }), SecureRandom())
+                }.socketFactory
+            }
 
             try {
                 connection.connect()
