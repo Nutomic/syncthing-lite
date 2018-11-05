19a8b66da744f5c1a6db9e881a210493c71d4044
Jonas L <jonas@determapp.de>
2018-11-05 15:11:58 +0100

    Clean up the syncthing client

--- syncthing-client/src/main/kotlin/net/syncthing/java/client/SyncthingClient.kt
+++ syncthing-client/src/main/kotlin/net/syncthing/java/client/SyncthingClient.kt
@@ -19,29 +19,15 @@ import kotlinx.coroutines.experimental.runBlocking
 import net.syncthing.java.bep.*
 import net.syncthing.java.bep.connectionactor.ConnectionActorGenerator
 import net.syncthing.java.bep.connectionactor.ConnectionActorWrapper
-import net.syncthing.java.core.beans.DeviceAddress
 import net.syncthing.java.core.beans.DeviceId
-import net.syncthing.java.core.beans.DeviceInfo
 import net.syncthing.java.core.beans.FileInfo
 import net.syncthing.java.core.configuration.Configuration
 import net.syncthing.java.core.interfaces.IndexRepository
 import net.syncthing.java.core.interfaces.TempRepository
-import net.syncthing.java.core.security.KeystoreHandler
-import net.syncthing.java.core.utils.awaitTerminationSafe
 import net.syncthing.java.discovery.DiscoveryHandler
-import org.slf4j.LoggerFactory
 import java.io.Closeable
-import java.io.IOException
 import java.io.InputStream
-import java.util.Collections
-import java.util.TreeSet
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
-import kotlin.collections.component1
-import kotlin.collections.component2
-import kotlin.collections.set
+import java.util.*
 
 class SyncthingClient(
         private val configuration: Configuration,
@@ -49,21 +35,11 @@ class SyncthingClient(
         private val tempRepository: TempRepository
 ) : Closeable {
 
-    private val logger = LoggerFactory.getLogger(javaClass)
-    val discoveryHandler: DiscoveryHandler
-    val indexHandler: IndexHandler
-    private val connections = Collections.synchronizedSet(createConnectionsSet())
-    private val connectByDeviceIdLocks = Collections.synchronizedMap(HashMap<DeviceId, Object>())
+    val indexHandler = IndexHandler(configuration, repository, tempRepository)
+    val discoveryHandler = DiscoveryHandler(configuration)
+
+    // TODO: call these listeners
     private val onConnectionChangedListeners = Collections.synchronizedList(mutableListOf<(DeviceId) -> Unit>())
-    private var connectDevicesScheduler = Executors.newSingleThreadScheduledExecutor()
-
-    private fun createConnectionsSet() = TreeSet<ConnectionHandler>(compareBy { it.address.score })
-
-    init {
-        indexHandler = IndexHandler(configuration, repository, tempRepository)
-        discoveryHandler = DiscoveryHandler(configuration)
-        connectDevicesScheduler.scheduleAtFixedRate(this::updateIndexFromPeers, 0, 15, TimeUnit.SECONDS)
-    }
 
     private val requestHandlerRegistry = RequestHandlerRegistry()
     private val newConnections = Connections(
@@ -91,7 +67,7 @@ class SyncthingClient(
         indexHandler.clearIndex()
         configuration.folders = emptySet()
         configuration.persistLater()
-        updateIndexFromPeers()
+        // TODO: update index from peers by reconnecting
     }
 
     fun addOnConnectionChangedListener(listener: (DeviceId) -> Unit) {
@@ -108,118 +84,6 @@ class SyncthingClient(
     init {
         discoveryHandler.newDeviceAddressSupplier() // starts the discovery
         getConnections()
-    }
-
-    @Throws(IOException::class, KeystoreHandler.CryptoException::class)
-    private fun openConnection(deviceAddress: DeviceAddress): ConnectionHandler {
-        logger.debug("Connecting to ${deviceAddress.deviceId}, active connections: ${connections.map { it.deviceId().deviceId }}")
-        val connectionHandler = ConnectionHandler(
-                configuration, deviceAddress, indexHandler, tempRepository, { connectionHandler, _ ->
-                    connectionHandler.close()
-                    openConnection(deviceAddress)
-                },
-                {connection ->
-                    if (!connection.isConnected) {
-                        connections.remove(connection)
-                    }
-                    onConnectionChangedListeners.forEach { it(connection.deviceId()) }
-                })
-
-        try {
-          connectionHandler.connect()
-        } catch (ex: Exception) {
-          connectionHandler.closeBg()
-
-          throw ex
-        }
-
-        connections.add(connectionHandler)
-
-        return connectionHandler
-    }
-
-    /**
-     * Takes discovered addresses from [[DiscoveryHandler]] and connects to devices.
-     *
-     * We need to make sure that we are only connecting once to each device.
-     */
-    private fun getPeerConnections(listener: (connection: ConnectionHandler) -> Unit, completeListener: () -> Unit) {
-        completeListener()
-        return
-
-        // create an copy to prevent dispatching an action two times
-        val connectionsWhichWereDispatched = createConnectionsSet()
-
-        synchronized (connections) {
-          connectionsWhichWereDispatched.addAll(connections)
-        }
-
-        connectionsWhichWereDispatched.forEach { listener(it) }
-
-        discoveryHandler.newDeviceAddressSupplier()
-                .takeWhile { it != null }
-                .filterNotNull()
-                .groupBy { it.deviceId() }
-                .filterNot { it.value.isEmpty() }
-                .forEach { (deviceId, addresses) ->
-                    // create an lock per device id to prevent multiple connections to one device
-
-                    synchronized (connectByDeviceIdLocks) {
-                      if (connectByDeviceIdLocks[deviceId] == null) {
-                        connectByDeviceIdLocks[deviceId] = Object()
-                      }
-                    }
-
-                    synchronized (connectByDeviceIdLocks[deviceId]!!) {
-                      val existingConnection = connections.find { it.deviceId() == deviceId && it.isConnected }
-
-                      if (existingConnection != null) {
-                        connectionsWhichWereDispatched.add(existingConnection)
-                        listener(existingConnection)
-
-                        return@synchronized
-                      }
-
-                      // try to use all addresses
-                      for (address in addresses.distinctBy { it.address }) {
-                        try {
-                          val newConnection = openConnection(address)
-
-                          connectionsWhichWereDispatched.add(newConnection)
-                          listener(newConnection)
-
-                          break  // it worked, no need to try more
-                        } catch (e: IOException) {
-                          logger.warn("error connecting to device = $address", e)
-                        } catch (e: KeystoreHandler.CryptoException) {
-                          logger.warn("error connecting to device = $address", e)
-                        }
-                      }
-                    }
-                }
-
-        // use all connections which were added in the time between and were not added by this function call
-        val newConnectionsBackup = createConnectionsSet()
-
-        synchronized (connections) {
-          newConnectionsBackup.addAll(connections)
-        }
-
-        connectionsWhichWereDispatched.forEach { newConnectionsBackup.remove(it) }
-
-        newConnectionsBackup.forEach { listener(it) }
-
-        completeListener()
-    }
-
-    private fun updateIndexFromPeers() {
-        getPeerConnections({ connection ->
-            try {
-                indexHandler.waitForRemoteIndexAcquired(connection)
-            } catch (ex: InterruptedException) {
-                logger.warn("exception while waiting for index", ex)
-            }
-        }, {})
     }
 
     fun getActiveConnectionsForFolder(folderId: String) = configuration.peerIds
@@ -250,23 +114,18 @@ class SyncthingClient(
         )
     }
 
-    fun getPeerStatus(): List<DeviceInfo> {
-        return configuration.peers.map { device ->
-            val isConnected = connections.find { it.deviceId() == device.deviceId }?.isConnected ?: false
-            device.copy(isConnected = isConnected)
-        }
+    fun getPeerStatus() = configuration.peers.map { device ->
+        device.copy(
+                isConnected = newConnections.getByDeviceId(device.deviceId).isConnected
+        )
     }
 
     override fun close() {
-        connectDevicesScheduler.awaitTerminationSafe()
         discoveryHandler.close()
-        // Create copy of list, because it will be modified by handleConnectionClosedEvent(), causing ConcurrentModificationException.
-        ArrayList(connections).forEach{it.close()}
         indexHandler.close()
         repository.close()
         tempRepository.close()
         newConnections.shutdown()
         assert(onConnectionChangedListeners.isEmpty())
     }
-
 }
