2743e8cd03add13517918aaf101ccd6af998b11b
Jonas L <jonas@determapp.de>
2018-11-05 10:30:56 +0100

    Remove unneeded locking

--- syncthing-bep/src/main/kotlin/net/syncthing/java/bep/connectionactor/ConnectionActorWrapper.kt
+++ syncthing-bep/src/main/kotlin/net/syncthing/java/bep/connectionactor/ConnectionActorWrapper.kt
@@ -6,12 +6,9 @@ import kotlinx.coroutines.experimental.channels.ReceiveChannel
 import kotlinx.coroutines.experimental.channels.SendChannel
 import kotlinx.coroutines.experimental.channels.consumeEach
 import kotlinx.coroutines.experimental.launch
-import kotlinx.coroutines.experimental.sync.Mutex
-import kotlinx.coroutines.experimental.sync.withLock
 
 class ConnectionActorWrapper (private val source: ReceiveChannel<SendChannel<ConnectionAction>>) {
     private val job = Job()
-    private val statusLock = Mutex()
 
     private var currentConnectionActor: SendChannel<ConnectionAction>? = null
 
@@ -21,9 +18,7 @@ class ConnectionActorWrapper (private val source: ReceiveChannel<SendChannel<Con
     init {
         GlobalScope.launch (job) {
             source.consumeEach { connectionActor ->
-                statusLock.withLock {
-                    currentConnectionActor = connectionActor
-                }
+                currentConnectionActor = connectionActor
             }
         }
     }
