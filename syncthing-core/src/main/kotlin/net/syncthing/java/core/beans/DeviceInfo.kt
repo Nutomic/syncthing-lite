b9458f8d2be45c549b56bd82c6a8001660838c08
Jonas L <jonas@determapp.de>
2018-12-05 15:25:13 +0100

    Remove obsolete isConnected from DeviceInfo

--- syncthing-core/src/main/kotlin/net/syncthing/java/core/beans/DeviceInfo.kt
+++ syncthing-core/src/main/kotlin/net/syncthing/java/core/beans/DeviceInfo.kt
@@ -17,7 +17,7 @@ package net.syncthing.java.core.beans
 import com.google.gson.stream.JsonReader
 import com.google.gson.stream.JsonWriter
 
-data class DeviceInfo(val deviceId: DeviceId, val name: String, val isConnected: Boolean? = null) {
+data class DeviceInfo(val deviceId: DeviceId, val name: String) {
 
     companion object {
         private const val DEVICE_ID = "deviceId"
@@ -43,9 +43,6 @@ data class DeviceInfo(val deviceId: DeviceId, val name: String, val isConnected:
             )
         }
     }
-
-    constructor(deviceId: DeviceId, name: String?) :
-            this(deviceId, if (name != null && !name.isBlank()) name else deviceId.shortId, null)
 
     fun serialize(writer: JsonWriter) {
         writer.beginObject()
